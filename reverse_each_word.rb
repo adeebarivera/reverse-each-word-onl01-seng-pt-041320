88564bb5ff5163f7807c8f073c1b89d3eb8ce150
adeeba reaz <adeebarivera@outlook.com>
2020-04-23 19:53:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 # Write a method called `reverse_each_word` that takes in a string argument of a sentence and returns that same sentence with each word reversed in place.
 def reverse_each_word(sentence)
-  
-  
+  words_array = sentence.split(" ") 
+  new_array = []
+  new_array = words_array.collect do |word|
 end
