695e2a2dc00ec648853c4892c1357e081489e7a5
adeeba reaz <adeebarivera@outlook.com>
2020-04-23 20:10:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,7 +6,7 @@ def reverse_each_word(sentence)
   words_array = sentence.split(" ") 
   new_array = []
   new_array = words_array.collect {|word| word.reverse}
-  new_array.
+  new_array.join(" ")
 end  
 
 
